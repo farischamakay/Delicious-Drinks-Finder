4a8d0b9ccadba30513e200b80fa1131cff34d076
Farischa Makay <57443923+farischamakay@users.noreply.github.com>
2021-11-29 21:24:34 +0700

    Add files via upload

---
+++ src/script/data/drink.js
@@ -0,0 +1,33 @@
+const drink = [
+    {
+        title : "Ace",
+        cover: "https://www.thecocktaildb.com/images/media/drink/l3cd7f1504818306.jpg",
+        description: "Shake all the ingredients in a cocktail shaker and ice then strain in a cold glass."
+    },
+    {
+        title: "Adam",
+        cover: "https://www.thecocktaildb.com/images/media/drink/v0at4i1582478473.jpg",
+        description: "In a shaker half-filled with ice cubes, combine all of the ingredients. Shake well. Strain into a cocktail glass."
+    },
+    {
+        title: "Melya",
+        cover: "https://www.thecocktaildb.com/images/media/drink/xwtptq1441247579.jpg",
+        description: "Brew espresso. In a coffee mug, place 1 teaspoon of unsweetened powdered cocoa, then cover a teaspoon with honey and drizzle it into the cup. Stir while the coffee brews, this is the fun part. The cocoa seems to coat the honey without mixing, so you get a dusty, sticky mass that looks as though it will never mix. Then all at once, presto! It looks like dark chocolate sauce. Pour hot espresso over the honey, stirring to dissolve. Serve with cream.",
+    },
+    {
+        title: "Orgasm",
+        cover: "https://www.thecocktaildb.com/images/media/drink/vr6kle1504886114.jpg",
+        description: "Shake all ingredients with ice, strain into a chilled cocktail glass, and serve.",
+    },
+    {
+        title: "Boxcar",
+        cover: "https://www.thecocktaildb.com/images/media/drink/pwgtpa1504366376.jpg",
+        description: "In a shaker half-filled with ice cubes, combine all of the ingredients. Shake well. Strain into a sour glass."
+    },
+    {
+        title: "Affair",
+        cover: "https://www.thecocktaildb.com/images/media/drink/h5za6y1582477994.jpg",
+        description: "Pour schnapps, orange juice, and cranberry juice over ice in a highball glass. Top with club soda and serve."
+    }
+];
+export default drink;
